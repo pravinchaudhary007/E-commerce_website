5abc40a23cbdf168dcb07f1223fb7ad3558812de
pravinchaudhary007 <chaudharypravin083@gmail.com>
2024-08-21 00:18:42 +0530

    Remove all files

--- src/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-
-
-const NotFound = () => {
-  return <div className="h-screen flex justify-center items-center text-center text-xl font-bold">404 - Page Not Found</div>;
-};
-
-export default NotFound;
