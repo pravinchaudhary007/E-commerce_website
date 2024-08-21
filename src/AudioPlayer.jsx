9a7363af1886fbf4a5761099faead336f63cf616
pravinchaudhary007 <chaudharypravin083@gmail.com>
2024-08-21 11:32:02 +0530

    Describe the changes you made

--- src/AudioPlayer.jsx
+++ src/AudioPlayer.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef } from 'react';
 import bgSound from './assets/bgSound.mp3';
-import bgSound2 from './assets/bgSound2.ogg';
 
 const AudioPlayer = () => {
   const audioRef = useRef(null);
@@ -36,7 +35,6 @@ const AudioPlayer = () => {
     <div>
       <audio ref={audioRef} preload="auto" loop muted>
         <source src={bgSound} type="audio/mp3" />
-        <source src={bgSound2} type="audio/ogg" />
         Your browser does not support the audio element.
       </audio>
     </div>
--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -1,7 +1,7 @@
 
 
 const NotFound = () => {
-  return <div className="h-screen flex justify-center items-center text-center text-xl font-bold">404 - Page Not Found</div>;
+  return <div className="h-screen flex justify-center items-center text-center lg:text-4xl md:text-2xl sm:text-xl text-lg font-bold">404 - Page Not Found</div>;
 };
 
 export default NotFound;
